a7a542555766a0e42f43a5ca347eec4679f100a1
adam n <adam.nawroc@gmail.com>
2018-03-01 11:52:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+
+def prime?(num)
+
+  # range of numbers
+  # loop through numbers
+  # if divisible by anything but itself, not prime
+
+  # if num == 2
+  #   return true
+  # elsif num > 1 and num.even?
+  #   return false
+  # elsif num > 1 and num.odd?
+  #   return true
+  # else
+  #   false
+  #
+  # end
+
+
+end
+
+p prime?(1763)
